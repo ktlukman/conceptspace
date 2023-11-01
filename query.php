5da716ea21a7464d51a9d6e0a06401805ac8fd7d
ktlukman <52275260+ktlukman@users.noreply.github.com>
2023-11-01 12:21:36 +0530

    Add files via upload

--- query.php
+++ query.php
@@ -1,34 +1,23 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Retrieve form data
-    $name = $_POST['name'];
-    $lastName = $_POST['lastName'];
-    $email = $_POST['email'];
-    $phoneNumber = $_POST['phoneNumber'];
-    $message = $_POST['message'];
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Collect form data
+    $name = $_POST["name"];
+    $lastName = $_POST["lastName"];
+    $email = $_POST["email"];
+    $phoneNumber = $_POST["phoneNumber"];
+    $message = $_POST["message"];
 
-    // Construct the email message
-    $to = 'ktlukman@gmail.com'; // Replace with the recipient's email address
-    $subject = 'Contact Form Submission from ' . $name;
-    $messageBody = "Name: $name\n";
-    $messageBody .= "Last Name: $lastName\n";
-    $messageBody .= "Email: $email\n";
-    $messageBody .= "Phone Number: $phoneNumber\n";
-    $messageBody .= "Message:\n$message";
+    // Send an email (You should configure your server's email settings)
+    $to = "ktlukman@gmail.com"; // Replace with the recipient's email address
+    $subject = "New Form Submission from $name $lastName";
+    $messageBody = "Name: $name $lastName\nEmail: $email\nPhone Number: $phoneNumber\nMessage: $message";
 
-    $headers = 'From: ktlukman@gmail.com' . "\r\n" .
-        'Reply-To: ' . $email . "\r\n";
-
-    // Send the email
-    if (mail($to, $subject, $messageBody, $headers)) {
-        // Email sent successfully
+    if (mail($to, $subject, $messageBody)) {
         echo "Email sent successfully!";
     } else {
-        // Email sending failed
-        echo "Email sending failed.";
+        echo "Failed to send email.";
     }
 } else {
-    // If the request is not POST, return an error or redirect to the form page.
-    echo "Invalid request";
+    echo "Invalid request.";
 }
 ?>
