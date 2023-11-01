11d5e84d2b80f8b221b464defb0a3ce4082d9697
ktlukman <52275260+ktlukman@users.noreply.github.com>
2023-11-01 12:23:49 +0530

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,39 +1,38 @@
 $(document).ready(function(){
     var gl1Height = $(".gl-1").height();
 	//console.log(gl1Height);
+	
 	$(".ourworks").css({"min-height":gl1Height+"px"});
 	
 	var gl2Height = $(".news-image").height();
 	
-	$(".hello-form").css({"min-height":gl2Height+"px"});
+	$(".hello-form").css({"min-height":gl2Height+"px"});	
 	
-	    $(".hello-form").submit(function(e) {
-			//e.preventDefault();
-        // Retrieve form data
-        var name = $("input[name='name']").val();
-        var lastName = $("input[name='lastName']").val();
-        var email = $("input[name='email']").val();
-        var phoneNumber = $("input[name='phoneNumber']").val();
-        var message = $("textarea[name='message']").val();
-
-        // Send data to the PHP backend using AJAX
-        $.ajax({
+	$("#contact-form").submit(function(event) {
+		event.preventDefault();
+		
+		
+        
+        if (this.checkValidity()) {
+          // Serialize the form data
+          var formData = $(this).serialize();
+          
+          // Send a POST request to the PHP script
+          $.ajax({
             type: "POST",
-            url: "./query.php", // The URL of your PHP script
-            data: {
-                name: name,
-                lastName: lastName,
-                email: email,
-                phoneNumber: phoneNumber,
-                message: message
-            },
+            url: "./query.php", // Replace with your PHP script URL
+            data: formData,
             success: function(response) {
-                // Handle the response from the server, e.g., display a success message
-                alert("Form submitted successfully. Server response:\n" + response);
+              // Handle the response from the PHP script (e.g., display a success message)
+              console.log("s"+response);
             }
-        });
-    });
+          });
+        }
+      });
+	
 
+	
+	
 });
 
 
